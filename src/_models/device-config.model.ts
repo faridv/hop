24d1c108d8bee4330d357a4a6b5c02fce223fdfe
Farid Rn <faridv@gmail.com>
2018-09-17 18:20:37 +0430

    Added default DeviceConfig model class

---
+++ src/_models/device-config.model.ts
@@ -0,0 +1,13 @@
+export class DeviceConfig {
+    channelList: object;
+    channelName: string;
+    channelCCID: string;
+    channelDescription: string;
+    channelONID: number;
+    channelSID: number;
+    channelTSID: number;
+    cookieEnabled: boolean;
+    userAgent: string;
+    viewportWidth: number;
+    viewportHeight: number;
+}
