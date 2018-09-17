c6b5b1a61899b43a6d0d9ca178cba0bd855edc7d
Farid Rn <faridv@gmail.com>
2018-09-17 18:18:20 +0430

    Moved index.ts to main.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { MyLibrary } from './MyLibrary';
-
-console.log('See this in your browser console: Typescript Webpack Starter Launched');
-
-const myLibrary = new MyLibrary();
-const result = myLibrary.executeDependency();
-const test = 'test';
-console.log(`A random number ${result}`);
-console.error(test);
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import App from './app/bootstrap';
+import * as Config from './../config.json';
+
+// Bootstrap AppManager
+new App(Config);
