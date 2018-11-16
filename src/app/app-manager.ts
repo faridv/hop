6336307b10c7b4f1c6e5a0f1f35a6a07c14f6b3b
Farid Rn <faridv@gmail.com>
2018-11-17 00:47:00 +0330

    Clean up and formatting + setKeys

--- src/app/app-manager.ts
+++ src/app/app-manager.ts
@@ -13,14 +13,14 @@ export default class AppManager {
     private template;
     private _bootstrapInstance: Bootstrap;
 
-    constructor (appData, Config, BootstrapInstance: Bootstrap) {
+    constructor(appData, Config, BootstrapInstance: Bootstrap) {
         this.config = Config;
         this.input = Inputs.instance;
         this.template = TemplateHelper.instance;
         this.container = Config.container;
         this._bootstrapInstance = BootstrapInstance;
         const self = this;
-        $(function() {
+        $(function () {
             self.$el = $(self.container);
             self.showButton(appData);
         });
@@ -31,15 +31,26 @@ export default class AppManager {
         const controlType = (appData.hasHub) ? 'button' : appData.layout;
         const templatePromise = this.template.load('controls', controlType);
         const button = appData.button;
-        this.template.render(templatePromise, appData, this.$el, 'append', function(element) {
-            const inputParams = {key: 'app.' + button.key, title: 'init'};
-            self.input.addEvent(button.key, true, inputParams, function() {
-                self.initializeApplication(appData);
-            });
+        this.template.render(templatePromise, appData, this.$el, 'append', function (element) {
 
+            // Show button after intentional delay time
             setTimeout(() => {
+
                 $(element).find('[class*="button-"]').addClass('show');
-            }, self.config.timeout);
+
+                // Add application initialization key event
+                const inputParams = {key: 'app.' + button.key, title: 'init'};
+                self.input.addEvent(button.key, true, inputParams, function () {
+                    self.initializeApplication(appData);
+                });
+
+                // Hide button after the configured time
+                setTimeout(() => {
+                    if ($(element).find('.show[class*="button-"]').length) {
+                        $(element).find('[class*="button-"]').removeClass('show');
+                    }
+                }, self.config.timeout);
+            }, self.config.delay);
         });
     }
 
@@ -49,14 +60,16 @@ export default class AppManager {
         const templatePromise = this.template.load('layouts', layout);
         const modules = appData.modules;
 
-        this.template.render(templatePromise, modules, this.$el, 'html', function() {
+        this._bootstrapInstance.setKeySet(0x1+0x2+0x4+0x8+0x10+0x20+0x40+0x80); // All Keys
+
+        this.template.render(templatePromise, modules, this.$el, 'html', function () {
             self.template.addClass('active');
 
             // Should be on body
             self.template.addClass('layout-' + layout, 'body');
 
             const inputParams = {key: 'app.close', title: 'خروج', button: true};
-            self.input.addEvent(appData.button.key, true, inputParams, function() {
+            self.input.addEvent(appData.button.key, true, inputParams, function () {
                 self.template.removeClassIfContains('body', 'layout-')
                 self._bootstrapInstance.destroy(layout);
             });
