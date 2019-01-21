5a61027986ab9f49befe4a8a98a470d047848e59
HbbTV Development <faridrdev@gmail.com>
2019-01-21 15:09:32 +0330

    Implementing new yahoo weather

--- src/modules/weather/weather.service.ts
+++ src/modules/weather/weather.service.ts
@@ -1,5 +1,5 @@
 import httpHelper from "../../_helpers/http.helper";
-import {ApiHelper} from "../../_helpers/api.helper";
+import { ApiHelper } from "../../_helpers/api.helper";
 
 export class WeatherService {
     private static _instance: WeatherService;
@@ -10,7 +10,34 @@ export class WeatherService {
     }
 
     getCity(coordinates) {
-        return this.http.get(ApiHelper.get('weather'), {format: 'json', q: this.yqlQuery.replace(/{coordinates}/g, coordinates)});
+        var OAuth = require('oauth');
+        var header = { "Yahoo-App-Id": "ngWTui6o" };
+        var coords = {lat: coordinates.split(',')[0], lon: coordinates.split(',')[1]};
+        var request = new OAuth.OAuth(
+            null,
+            null,
+            'dj0yJmk9MVRhZE1FcHE5dFBCJmQ9WVdrOWJtZFhWSFZwTm04bWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD03NA--',
+            'e19c8e0260309d86ceb0d4e1aa78e892a243bd9d',
+            '1.0',
+            null,
+            'HMAC-SHA1',
+            null,
+            header
+        );
+        return request.get(
+            'https://weather-ydn-yql.media.yahoo.com/forecastrss?lat=' + coords.lat + '&lon=' + coords.lon + '&format=json&u=c',
+            null,
+            null,
+            function (err, data, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data)
+                }
+            }
+        );
+
+        // return this.http.get(ApiHelper.get('weather'), { format: 'json', q: this.yqlQuery.replace(/{coordinates}/g, coordinates) });
     }
 
     public static get instance() {
