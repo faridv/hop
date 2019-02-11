f7f72ae7a6eaa19faaac9e900c48b009558d4d57
Farid Rn <faridv@gmail.com>
2019-02-12 01:43:54 +0330

    Working on weather module services

--- src/modules/weather/weather.service.ts
+++ src/modules/weather/weather.service.ts
@@ -1,43 +1,25 @@
 import httpHelper from "../../_helpers/http.helper";
-import { ApiHelper } from "../../_helpers/api.helper";
+import {ApiHelper} from "../../_helpers/api.helper";
 
 export class WeatherService {
     private static _instance: WeatherService;
     private http = httpHelper;
-    private yqlQuery = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="({coordinates})") and u="c"';
+    private appId = '0b692e71250c1114ced02ee70446131d';
 
     constructor() {
     }
 
     getCity(coordinates) {
-        var OAuth = require('oauth');
-        var header = { "Yahoo-App-Id": "ngWTui6o" };
-        var coords = {lat: coordinates.split(',')[0], lon: coordinates.split(',')[1]};
-        var request = new OAuth.OAuth(
-            null,
-            null,
-            'dj0yJmk9MVRhZE1FcHE5dFBCJmQ9WVdrOWJtZFhWSFZwTm04bWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD03NA--',
-            'e19c8e0260309d86ceb0d4e1aa78e892a243bd9d',
-            '1.0',
-            null,
-            'HMAC-SHA1',
-            null,
-            header
-        );
-        return request.get(
-            'https://weather-ydn-yql.media.yahoo.com/forecastrss?lat=' + coords.lat + '&lon=' + coords.lon + '&format=json&u=c',
-            null,
-            null,
-            function (err, data, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(data)
-                }
-            }
-        );
-
-        // return this.http.get(ApiHelper.get('weather'), { format: 'json', q: this.yqlQuery.replace(/{coordinates}/g, coordinates) });
+        const coords = {
+            lat: coordinates.split(',')[0],
+            lon: coordinates.split(',')[1]
+        };
+        return this.http.get(ApiHelper.get('weather'), {
+            lat: coords.lat,
+            lon: coords.lon,
+            units: 'metric',
+            APPID: this.appId
+        });
     }
 
     public static get instance() {
