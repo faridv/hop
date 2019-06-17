274884aa776947819faf2623d937a7ea99cc4789
Farid Rn <faridv@gmail.com>
2019-06-17 21:34:29 +0430

    removed LogRocket for now

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import * as Config from './../config.json';
 import App from './app/bootstrap';
 
-import * as LogRocket from 'logrocket';
-LogRocket.init('pdu5s5/hop');
-LogRocket.identify('1', {
-    name: 'Farid Rn',
-    email: 'faridv@gmail.com',
-    type: 'admin'
-});
+// import * as LogRocket from 'logrocket';
+// LogRocket.init('pdu5s5/hop');
+// LogRocket.identify('1', {
+//     name: 'Farid Rn',
+//     email: 'faridv@gmail.com',
+//     type: 'admin'
+// });
 
 // Bootstrap AppManager
 new App(Config);
