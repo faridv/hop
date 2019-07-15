498fe53e95202471662b92636183ae73fc07417f
Farid Rn <faridv@gmail.com>
2019-07-15 13:52:26 +0430

    updated program model

--- src/modules/program/program.model.ts
+++ src/modules/program/program.model.ts
@@ -1,45 +1,19 @@
 export class Program {
-    Created: string;
-    Director: string;
-    EpisodesCount: number;
-    EpisodesList: any;
-    FullText: string;
-    Genre: number;
-    Id: number;
-    Image: string;
-    IntroText: string;
-    IsPublished: number;
-    Kind: number;
-    MetaCategoryId: number;
-    Priority: number;
-    Produced: string;
-    Published: string;
-    Schedule: string;
-    Status: string;
-    Title: string;
-    Video: string;
-    VideoThumbnail: any;
-    ViewCount: string;
-    Youtube: string;
+    id: number;
+    image: string;
+    summary: string;
+    title: string;
+    description: string;
+    director: string;
+    schedule: string;
+    trailer: string;
 }
 
 export class ProgramEpisode {
-    Created: string;
-    Fulltext: any;
-    Id: number;
-    Image: string;
-    Introtext: string;
-    IsPublished: number;
-    MediaId: null;
-    Number: number;
-    Pid: number;
-    Priority: number;
-    Produced: string;
-    Published: string;
-    ShortUrl: string;
-    Title: string;
-    Video: string;
-    VideoThumbnail: any;
-    ViewCount: string;
-    Youtube: string;
+    id: number;
+    image: string;
+    video: string;
+    title: string;
+    summary: string;
+    part: number;
 }
