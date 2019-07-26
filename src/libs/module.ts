54f1a059a651182b997518f4d5ea6f4c626062db
Farid Rn <faridv@gmail.com>
2019-07-27 01:56:44 +0430

    working on module base class

--- src/libs/module.ts
+++ src/libs/module.ts
@@ -1,17 +1,22 @@
 import Inputs from '../app/inputs';
 import TemplateHelper from '../_helpers/template.helper';
+import Layouts from '../app/layouts';
 
 export class Module {
 
     protected service;
+    protected config;
     protected template: TemplateHelper;
     protected input: Inputs;
     protected $el = $('#content');
     protected events = {};
+    protected layoutInstance: Layouts;
 
     constructor(config?, layoutInstance?) {
+        this.config = config;
         this.template = TemplateHelper.instance;
         this.input = Inputs.instance;
+        this.layoutInstance = layoutInstance;
     }
 
     load(...args: any): void {
@@ -22,12 +27,16 @@ export class Module {
 
     }
 
-    destroy(instance?: any): boolean {
-        const self = typeof instance !== 'undefined' ? instance : this;
+    destroyEvents(instance: any): boolean {
         for (let item in this.events) {
-            self.input.removeEvent(this.events[item]['control'], {key: item});
+            instance.input.removeEvent(this.events[item]['control'], {key: item});
         }
         return true;
+    }
+
+    destroy(instance?: any): boolean {
+        const self = typeof instance !== 'undefined' ? instance : this;
+        return this.destroyEvents(self);
     }
 
 }
