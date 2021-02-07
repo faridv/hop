ab77666d4457a712ef13b49eeecf43fc762205a3
Farid Rn <faridv@gmail.com>
2021-02-07 15:19:12 +0330

    added scss auto-compilation config
    and updated config to match the latest version of Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
 
 const buildDate = new Date().toGMTString().slice(0, 25);
 const buildHash = (+new Date() / 1000).toFixed(0);
 
-const plugins = [
-    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /fa/),
-    new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify(nodeEnv)
-        }
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-        title: 'HOP: The HbbTV Open Platform',
-        description: 'HbbTV Open Platform built to run on HbbTV-ready TV sets.',
-        template: '!!ejs-loader!src/index.html',
-        direction: 'rtl',
-        language: 'fa-IR',
-        languageCode: 'fa',
-        modified: buildDate,
-        buildHash: buildHash,
-        hash: true,
-        inject: 'head',
-        cache: false,
-        pretty: true,
-        minify: false
-    }),
-    new CopyWebpackPlugin({
-        patterns: [
-            {from: './assets', to: 'assets/'},
-            {from: '.htaccess'}
-        ]
-    })
-];
-
-var config = {
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, 'dist/'),
+        historyApiFallback: true,
+        compress: false,
+        port: 3000,
+        hot: true,
+        inline: true
+    },
     devtool: isProd ? '' : 'source-map',
     // watch: !isProd,
     context: path.resolve('./src'),
-    entry: {
-        app: './main.ts'
-    },
-    node: {
-        __filename: true
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: ''
-    },
+    entry: [
+        // app: './main.ts'
+        './main.ts',
+        './scss/hop.scss',
+    ],
     module: {
         rules: [
             {
@@ -67,46 +38,113 @@ var config = {
             !isProd
                 ? {
                     test: /\.(js|ts)$/,
-                    loader: 'istanbul-instrumenter-loader',
-                    exclude: [/\/node_modules\//]
+                    exclude: [/\/node_modules\//],
+                    use: {
+                        loader: 'istanbul-instrumenter-loader',
+                    },
                 }
                 : null,
             {
                 test: /\.html$/,
-                loader: 'html-loader',
-                options: {
-                    minimize: false
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: false
+                    }
                 }
             },
             {
                 test: /\.js$/i,
-                loader: 'babel-loader',
-                options: {
-                    sourceType: "unambiguous",
-                }
+                exclude: /node_modules|assets/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        sourceType: "unambiguous",
+                        compact: true,
+                    },
+                },
             },
+            // {
+            //     test: /\.css$/,
+            //     // loaders: ['style-loader', 'css-loader']
+            //     loader: 'css-loader'
+            // }
             {
-                test: /\.css$/,
-                // loaders: ['style-loader', 'css-loader']
-                loader: 'css-loader'
-            }
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {url: false, importLoaders: 1},
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
         ].filter(Boolean)
     },
+    node: {
+        __filename: true
+    },
+    output: {
+        filename: '[name].[fullhash].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    plugins: [
+        // moment loads all locales by default ant it takes so much space
+        // This lines removes all locales except the mentioned ones.
+        // You can replace /fa/ with your desired locale to keep it.
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /fa/),
+        // Clean up webpack-created dist folder
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(nodeEnv)
+            }
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+
+            // title: 'HOP: The HbbTV Open Platform',
+            // description: 'HbbTV Open Platform built to run on HbbTV-ready TV sets.',
+            // template: '!!ejs-loader!src/index.html',
+            // direction: 'rtl',
+            // language: 'fa-IR',
+            // languageCode: 'fa',
+            // modified: buildDate,
+            // buildHash: buildHash,
+            // hash: true,
+            inject: 'head',
+            cache: false,
+            // pretty: true,
+            // minify: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[fullhash].css',
+            chunkFilename: '[id].[fullhash].css',
+        }),
+        // Copy static files directly without any process
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'assets', to: 'assets/'},
+                {from: '.htaccess'}
+            ]
+        })
+    ],
     resolve: {
         extensions: ['.ts', '.js'],
         alias: {
             handlebars: 'handlebars/dist/handlebars.min.js',
-            "handlebars/runtime": 'handlebars/dist/handlebars.runtime.min.js'
+            // "handlebars/runtime": 'handlebars/dist/handlebars.runtime.min.js'
         }
     },
-    plugins: plugins,
-    devServer: {
-        contentBase: path.join(__dirname, 'dist/'),
-        compress: false,
-        port: 3000,
-        hot: true,
-        inline: true
-    }
 };
 
-module.exports = config;
+// module.exports = config;
