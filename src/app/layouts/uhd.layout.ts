d2ffbb0f7cebda9c067e729d5bcfae13414221f3
Farid Rn <faridv@gmail.com>
2021-08-29 02:04:33 +0430

    back to home shall keep prev. menu item

--- src/app/layouts/uhd.layout.ts
+++ src/app/layouts/uhd.layout.ts
@@ -10,7 +10,7 @@ export default class UHDLayout {
 
         const input = Inputs.instance;
         // Remove back key if we are returning back from a module
-        input.removeEvent('back,backspace,b', {key: 'module.exit'});
+        input.removeEvent('back,backspace,b', { key: 'module.exit' });
 
         // Load Clock
         if (typeof appData.clock !== 'undefined' && appData.clock) {
@@ -28,6 +28,11 @@ export default class UHDLayout {
 
     public static initialize(config, $el, LayoutInstance: Layouts): void {
         UHDLayout.handleEvents($el, config, LayoutInstance);
+
+        // find previous active menu item
+        const previousActiveMenuIndex = $el.find('> li.active').index();
+        $el.find('li').removeClass('active');
+
         let slidesToShow = 7;
         if (!$el.is(':visible'))
             $el.show(1);
@@ -46,6 +51,12 @@ export default class UHDLayout {
             useCSS: false,
             useTransform: false
         });
+
+        // go to previous active menu if any available
+        if (previousActiveMenuIndex >= 0) {
+            $el.slick('slickGoTo', previousActiveMenuIndex, true);
+        }
+
         UHDLayout.handleKeys($el);
     }
 
@@ -65,7 +76,7 @@ export default class UHDLayout {
         $(document).on('click', "#menu ul li a", (e) => {
             UHDLayout.loadModule($carousel, config, LayoutInstance);
         });
-        const enterParams = {key: 'carousel.select', title: 'انتخاب برنامه', icon: 'tv', button: false};
+        const enterParams = { key: 'carousel.select', title: 'انتخاب برنامه', icon: 'tv', button: false };
         input.addEvent('enter', false, enterParams, () => {
             UHDLayout.loadModule($carousel, config, LayoutInstance);
         });
@@ -81,18 +92,22 @@ export default class UHDLayout {
 
     public static loadModule($carousel, config, LayoutInstance): void {
         const $currentSlide = $carousel.find('.slick-current').find('li:first');
+        $currentSlide.addClass('active');
         UHDLayout.destroy($carousel, config).then(() => {
-            LayoutInstance.loadModule($currentSlide.data('type'), config);
+            const url = typeof $currentSlide.data('url') !== 'undefined' && $currentSlide.data('url')
+                ? $currentSlide.data('url')
+                : null;
+            LayoutInstance.loadModule($currentSlide.data('type'), config, false, url);
         });
     }
 
     public static handleKeys($carousel): void {
         const input = Inputs.instance;
-        const leftParams = {key: 'carousel.left', title: 'چپ', icon: 'left', button: false};
+        const leftParams = { key: 'carousel.left', title: 'چپ', icon: 'left', button: false };
         input.addEvent('left', false, leftParams, () => {
             $carousel.slick('slickNext');
         });
-        const rightParams = {key: 'carousel.right', title: 'راست', icon: 'right', button: false};
+        const rightParams = { key: 'carousel.right', title: 'راست', icon: 'right', button: false };
         input.addEvent('right', false, rightParams, () => {
             $carousel.slick('slickPrev');
         });
@@ -100,9 +115,9 @@ export default class UHDLayout {
 
     public static unsetKeys(): void {
         const input = Inputs.instance;
-        input.removeEvent('left', {key: 'carousel.left'});
-        input.removeEvent('right', {key: 'carousel.right'});
-        input.removeEvent('enter', {key: 'carousel.select'});
+        input.removeEvent('left', { key: 'carousel.left' });
+        input.removeEvent('right', { key: 'carousel.right' });
+        input.removeEvent('enter', { key: 'carousel.select' });
         $(document).off('click', "#menu ul li a");
     }
 
