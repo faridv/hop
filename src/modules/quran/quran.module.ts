722a6c9dd5686b3e868d5c397a9b3babacc4fe8d
HbbTV Development <faridrdev@gmail.com>
2021-11-17 12:10:51 +0330

    fixed quran module keyboard scroll issue

--- src/modules/quran/quran.module.ts
+++ src/modules/quran/quran.module.ts
@@ -64,12 +64,12 @@ export default class QuranModule extends Module {
             $next.addClass('active');
             const distance = $next.offset().top - $el.offset().top + $el.scrollTop();
             if ($el.scrollTop() !== distance) {
-                $el.animate({ 'scrollTop': distance });
+                $el.animate({ 'scrollTop': distance }, 150);
             }
         } else {
             const $first = $el.find('li').eq(0);
             $first.addClass('active');
-            $el.animate({ 'scrollTop': 0 });
+            $el.animate({ 'scrollTop': 0 }, 150);
         }
     }
 
