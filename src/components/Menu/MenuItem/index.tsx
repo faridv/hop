c415c65c2add465eec41216775374dffa42f8451
Farid Rn <faridrdev@gmail.com>
2025-01-20 20:44:18 +0330

    Update index.tsx

--- src/components/Menu/MenuItem/index.tsx
+++ src/components/Menu/MenuItem/index.tsx
@@ -1,8 +1,7 @@
-import { NavLink, NavLinkRenderProps, useNavigate } from 'react-router-dom';
-import React, { RefObject } from 'react';
-import { MenuItemStyled } from './style';
-import { useFocusable } from '@noriginmedia/norigin-spatial-navigation';
-
+import { NavLink, NavLinkRenderProps, useNavigate } from "react-router-dom";
+import React, { RefObject } from "react";
+import { MenuItemStyled } from "./style";
+import { useFocusable } from "@noriginmedia/norigin-spatial-navigation";
 
 interface MenuItemProps {
   route: string;
@@ -12,20 +11,21 @@ interface MenuItemProps {
 }
 
 function MenuItem(props: MenuItemProps) {
-
   const navigate = useNavigate();
-  const { ref, focused }: { ref: RefObject<any>, focused: boolean } = useFocusable({
-    onEnterPress: (): void => {
-      navigate(`${props.route}`);
-    },
-  });
-
+  const { ref, focused }: { ref: RefObject<any>; focused: boolean } =
+    useFocusable({
+      onEnterPress: (): void => {
+        navigate(`${props.route}`);
+      },
+    });
 
   return (
     <MenuItemStyled ref={ref} focused={focused}>
       <NavLink
         to={`${props.route}`}
-        className={(isActive: NavLinkRenderProps) => isActive.isActive ? 'active' : ''}
+        className={(isActive: NavLinkRenderProps) =>
+          isActive.isActive ? "active" : ""
+        }
       >
         {props.children as any}
       </NavLink>
